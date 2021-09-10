269198c5e0693d72fff4d3c4276445afb96f362c
BwayCer <bwaycer@gmail.com>
2021-09-10 14:00:51 +0800

    feat(ht-gulp-tool): 導出動態 Pug 樣板

--- packages/ht-gulp-tool/src/plugin/pug.js
+++ packages/ht-gulp-tool/src/plugin/pug.js
@@ -13,15 +13,37 @@ import pug from 'pug';
  * @return {stream.Transform}
  */
 export function gulpPug(option) {
+  return _createGulpPugPipe(true, option);
+}
+
+/**
+ * Pug 轉 Script 管道。
+ *
+ * @func gulpPugScript
+ * @param {Object} [option]
+ * @return {stream.Transform}
+ */
+export function gulpPugScript(option) {
+  option.name = 'pugTemplate';
+  return _createGulpPugPipe(false, option);
+}
+
+function _createGulpPugPipe(isStatic, option) {
+  let extName = isStatic ? '.html' : '.js';
+  let memberName = isStatic ? 'renderFile' : 'compileFileClient';
+
   return new Transform({
     transform(file, encoding, callback) {
       let pathParse = path.parse(file.path);
       let outFilePath = path.join(
         pathParse.dir,
-        pathParse.name + '.html'
+        pathParse.name + extName
       );
       try {
-        let result = pug.renderFile(file.path, option);
+        let result = pug[memberName](file.path, option);
+        if (!isStatic)  {
+          result += '\nexport default pugTemplate;';
+        }
         file.path = outFilePath;
         file.contents = Buffer.from(result, encoding);
         callback(null, file);
